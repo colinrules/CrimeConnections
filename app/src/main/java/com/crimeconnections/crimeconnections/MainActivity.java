1738f7a50d498477881ab287f772b2ca5006a7f8
mackenzierandall <mackenzielrandall@gmail.com>
2018-08-30 16:50:51 -0700

    Added logo to action bar.
    Signed-off-by: mackenzierandall <mackenzielrandall@gmail.com>

--- app/src/main/java/com/crimeconnections/crimeconnections/MainActivity.java
+++ app/src/main/java/com/crimeconnections/crimeconnections/MainActivity.java
@@ -1,13 +1,25 @@
 package com.crimeconnections.crimeconnections;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //getSupportActionBar().setTitle("Crime Connect");
+
     }
 }
+
